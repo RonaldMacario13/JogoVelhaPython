f8c865d400f09507d467be73296a188a193a2bbe
Ronald Macario <ronaldmacario13@gmail.com>
2023-07-19 13:57:18 -0300

    feat: adicionando função para atualizar quadro

--- jogoVelha.py
+++ jogoVelha.py
@@ -2,3 +2,8 @@ def criar_quadro():
     quadro_linha = [["_", "_", "_"], ["_", "_", "_"], [" ", " ", " "]]
     mostrar_quadro(quadro_linha)
     return(quadro_linha)
+
+# def atualizar_quadro(quadro_atualizado):
+#     for linha in quadro_atualizado:
+#         print("|".join(linha))
+
